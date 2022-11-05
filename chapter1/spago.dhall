294524fa48b9c328a1d7f3ed07263e375ece4783
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-11-05 17:47:12 +0900

    use jelly

--- chapter1/spago.dhall
+++ chapter1/spago.dhall
@@ -15,6 +15,7 @@ to generate this file without the comments in this block.
   [ "aff"
   , "console"
   , "effect"
+  , "foldable-traversable"
   , "jelly"
   , "prelude"
   , "simple-hooks"
--- chapter1/src/Main.purs
+++ chapter1/src/Main.purs
@@ -2,9 +2,22 @@ module Main where
 
 import Prelude
 
+import Data.Foldable (traverse_)
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
+import Jelly.Aff (awaitBody)
+import Jelly.Component (class Component, text)
+import Jelly.Element as JE
+import Jelly.Hydrate (mount)
+import Signal.Hooks (runHooks_)
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main = launchAff_ do
+  bm <- awaitBody
+  liftEffect $ runHooks_ $ traverse_ (mount root) bm
+
+root :: forall m. Component m => m Unit
+root = do
+  JE.div' do
+    text "Hello World"
